760587794facde2181b12c995afa0ab6a412f870
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2023-01-28 19:46:06 +0800

    feat: создать роут для users

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { celebrate, Joi, Segments } = require('celebrate');
+
+users.get('/me', getCurrentUser);
+users.patch('/me', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().email(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), updateProfile);
