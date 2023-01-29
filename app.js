20125de6c96dc6e339f0b4689661beca46b7658b
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2023-01-29 12:21:08 +0800

    feat: добавить контроллер users и классы ошибок

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-// подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.listen(3000);
---
+++ controllers/users.js
@@ -0,0 +1,46 @@
+const { find } = require('../models/user')
+const User = require('../models/user')
+
+const NotFoundError = require('../errors/NotFoundError');
+const BadRequestError = require('../errors/BadRequestError');
+
+module.exports.getCurrentUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Нет пользователя с таким id');
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+}
+
+module.exports.updateProfile = (req, res, next) => {
+  const { email, name } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { email, name }, {
+    new: true,
+    runValidators: true,
+  })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Нет пользователя с таким id');
+      }
+      return res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else if (err.name === 'CastError') {
+        next(new BadRequestError('Некорректный id пользователя'));
+      } else {
+        next(err);
+      }
+    });
+};
